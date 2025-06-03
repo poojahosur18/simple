0d4243900a40afdd7ba8e1eefa1475f2ddc03a3f
poojahosur18 <poojahosur18gmail.com>
2025-06-03 10:38:47 +0530

    save

---
+++ SimpleInterest1.java
@@ -0,0 +1,17 @@
+package SimpleInsterest;
+
+public class SimpleInterest1 {
+    public static void main(String[] args) {
+        double principal = 5000;
+        double rate = 5;
+        double time = 2;
+
+        double interest = (principal * rate * time) / 100;
+
+        System.out.println("=== Simple Interest 1 ===");
+        System.out.println("Principal: " + principal);
+        System.out.println("Rate: " + rate + "%");
+        System.out.println("Time: " + time + " years");
+        System.out.println("Simple Interest: " + interest);
+    }
+}
